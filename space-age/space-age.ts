9da6bedf3effce007d9a5e531306a9e740c35888
Adam Pritchard <hello@adampritchard.com.au>
2023-01-06 08:21:25 +1000

    space-age; iteration 1

--- space-age/space-age.ts
+++ space-age/space-age.ts
@@ -1,3 +1,21 @@
-export function age(planet: unknown, seconds: unknown): unknown {
-  throw new Error('Remove this statement and implement this function')
+const earthSecondsPerYear = 31557600;
+
+const planetEarthYears = {
+  'mercury': 0.2408467,
+  'venus':   0.61519726,
+  'earth':   1.0,
+  'mars':    1.8808158,
+  'jupiter': 11.862615,
+  'saturn':  29.447498,
+  'uranus':  84.016846,
+  'neptune': 164.79132,
+};
+
+type Planet = keyof typeof planetEarthYears;
+
+export function age(planet: Planet, seconds: number): number {
+  const years = seconds / earthSecondsPerYear;
+  const result = years / planetEarthYears[planet];
+  const rounded = Math.round(result * 100) / 100;
+  return rounded;
 }
