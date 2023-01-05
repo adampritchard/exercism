19b29b0c0cb7e3a997578c789dc21cb40369a009
Adam Pritchard <hello@adampritchard.com.au>
2023-01-05 15:48:26 +1000

    rna-transcription; iteration 1

--- rna-transcription/rna-transcription.ts
+++ rna-transcription/rna-transcription.ts
@@ -1,3 +1,18 @@
-export function toRna() {
-  throw new Error('Remove this statement and implement this function')
+const dnaToRnaMap: Record<string, string> = {
+  'G': 'C',
+  'C': 'G',
+  'T': 'A',
+  'A': 'U',
+};
+
+export function toRna(dna: string): string {
+  let rna = '';
+
+  for (const dnaChar of dna) {
+    const rnaChar = dnaToRnaMap[dnaChar];
+    if (!rnaChar) throw new Error('Invalid input DNA.');
+    rna += rnaChar;
+  }
+
+  return rna;
 }
