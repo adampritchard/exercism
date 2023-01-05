6438997966bd7a094ad91928a17087ecd1eef06c
Adam Pritchard <hello@adampritchard.com.au>
2023-01-06 08:49:45 +1000

    dnd-character; iteration 1

--- dnd-character/dnd-character.ts
+++ dnd-character/dnd-character.ts
@@ -1,9 +1,34 @@
 export class DnDCharacter {
-  public static generateAbilityScore(): number {
-    throw new Error('Remove this statement and implement this function')
+  public strength: number;
+  public dexterity: number;
+  public constitution: number;
+  public intelligence: number;
+  public wisdom: number;
+  public charisma: number;
+  public hitpoints: number;
+
+  public constructor() {
+    this.strength = DnDCharacter.generateAbilityScore();
+    this.dexterity = DnDCharacter.generateAbilityScore();
+    this.constitution = DnDCharacter.generateAbilityScore();
+    this.intelligence = DnDCharacter.generateAbilityScore();
+    this.wisdom = DnDCharacter.generateAbilityScore();
+    this.charisma = DnDCharacter.generateAbilityScore();
+    this.hitpoints = 10 + DnDCharacter.getModifierFor(this.constitution);
   }
 
-  public static getModifierFor(abilityValue: number): number {
-    throw new Error('Remove this statement and implement this function')
+  public static generateAbilityScore(): number {
+    const rolls = [rollDie(), rollDie(), rollDie(), rollDie()];
+    rolls.sort();
+    return rolls[1] + rolls[2] + rolls[3];
   }
+
+  /** To calculate a modifier: subtract 10, divide by 2, then round down. */
+  public static getModifierFor(abilityValue: number): number {
+    return Math.floor((abilityValue - 10) / 2);
+  }
+}
+
+function rollDie(): number {
+  return Math.ceil(Math.random() * 6);
 }
