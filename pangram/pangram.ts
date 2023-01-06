e02621ba6aea214c03d92b1ba15510ed3344f0a4
Adam Pritchard <hello@adampritchard.com.au>
2023-01-07 08:03:32 +1000

    pangram; iteration 1

--- pangram/pangram.ts
+++ pangram/pangram.ts
@@ -1,3 +1,17 @@
-export function isPangram() {
-  throw new Error('Remove this statement and implement this function')
+export function isPangram(text: string): boolean {
+  text = text.toLowerCase();
+
+  const foundLetters: Record<string, boolean> = {};
+  for (let char of text) {
+    if (isLetter(char)) {
+      foundLetters[char] = true;
+    }
+  }
+
+  const foundCount = Object.keys(foundLetters).length;
+  return foundCount === 26;
+}
+
+function isLetter(char: string): boolean {
+  return char >= 'a' && char <= 'z';
 }
