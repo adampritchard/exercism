e6dabecc963e753e2a8fa8dad0e9af4da771b98c
Adam Pritchard <hello@adampritchard.com.au>
2023-01-07 08:12:37 +1000

    pangram; iteration 2

--- pangram/pangram.ts
+++ pangram/pangram.ts
@@ -1,17 +1,5 @@
 export function isPangram(text: string): boolean {
-  text = text.toLowerCase();
-
-  const foundLetters: Record<string, boolean> = {};
-  for (let char of text) {
-    if (isLetter(char)) {
-      foundLetters[char] = true;
-    }
-  }
-
-  const foundCount = Object.keys(foundLetters).length;
-  return foundCount === 26;
-}
-
-function isLetter(char: string): boolean {
-  return char >= 'a' && char <= 'z';
+  const lettersOnly = text.toLowerCase().replace(/[^a-z]/g, '');
+  const set = new Set(lettersOnly);
+  return set.size === 26;
 }
