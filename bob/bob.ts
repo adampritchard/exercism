bb4732d654b3a43bd3aedeeb7b5bc9cd3dd64918
Adam Pritchard <hello@adampritchard.com.au>
2023-01-07 09:42:24 +1000

    bob; iteration 1

--- bob/bob.ts
+++ bob/bob.ts
@@ -1,3 +1,12 @@
-export function hey(message: unknown): unknown {
-  throw new Error('Remove this statement and implement this function')
+export function hey(message: string): string {
+  message = message.trim();
+  
+  const isQuestion = message.endsWith('?');
+  const isYelling = message.match(/[A-Z]/) !== null && message.match(/[a-z]/) === null;
+
+  if (!message) return 'Fine. Be that way!';
+  if (isQuestion && isYelling) return "Calm down, I know what I'm doing!";
+  if (isQuestion) return 'Sure.';
+  if (isYelling) return 'Whoa, chill out!';
+  return 'Whatever.';
 }
