e256a74094a0901e9b1b0ba499cf16c578179aa5
Adam Pritchard <hello@adampritchard.com.au>
2023-01-07 12:29:06 +1000

    matrix; iteration 1

--- matrix/matrix.ts
+++ matrix/matrix.ts
@@ -1,13 +1,42 @@
 export class Matrix {
-  constructor() {
-    throw new Error('Remove this statement and implement this function')
+  protected values: number[][];
+
+  constructor(text: string) {
+    this.values = this.parseValues(text);
   }
 
-  get rows(): unknown {
-    throw new Error('Remove this statement and implement this function')
+  get rows(): number[][] {
+    return this.values;
   }
 
-  get columns(): unknown {
-    throw new Error('Remove this statement and implement this function')
+  get columns(): number[][] {
+    return this.transpose(this.values);
   }
+
+  protected parseValues(text: string): number[][] {
+    const vals: number[][] = [];
+
+    const lines = text.split('\n');
+    for (const line of lines) {
+      const chars = line.split(' ');
+      const row: number[] = chars.map(char => parseInt(char));
+      vals.push(row);
+    }
+
+    return vals;
+  }
+
+  protected transpose<T>(arr: T[][]): T[][] {
+    const transposed: T[][] = [];
+  
+    for (let [i, row] of arr.entries()) {
+      for (let [j, val] of row.entries()) {
+        if (!transposed[j]) transposed[j] = [];
+  
+        transposed[j][i] = val;
+      }
+    }
+  
+    return transposed;
+  }  
 }
