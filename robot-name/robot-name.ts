43d9fa705e15dde5479e68b90a10790c34cd9681
Adam Pritchard <hello@adampritchard.com.au>
2023-01-07 13:26:57 +1000

    robot-name; iteration 1

--- robot-name/robot-name.ts
+++ robot-name/robot-name.ts
@@ -1,15 +1,42 @@
 export class Robot {
-  constructor() {}
+  protected static usedNames = new Set<string>();
+  protected _name: string;
+
+  constructor() {
+    this._name = this.generateName();
+  }
 
   public get name(): string {
-    throw new Error('Implement Robot#name')
+    return this._name;
   }
 
   public resetName(): void {
-    throw new Error('Implement Robot#resetName')
+    this._name = this.generateName();
   }
 
   public static releaseNames(): void {
-    throw new Error('Implement Robot.releaseNames')
+    Robot.usedNames.clear();
   }
+
+  protected generateName(): string {
+    // Loop until we find a unique name.
+    let name: string;
+    do {
+      name = `${randomLetter()}${randomLetter()}${randomNumber()}${randomNumber()}${randomNumber()}`;
+    } while (Robot.usedNames.has(name));
+
+    Robot.usedNames.add(name);
+
+    return name;
+  }
+}
+
+function randomLetter(): string {
+  const index = Math.floor(Math.random() * 26);
+  const code = 65 + index;
+  return String.fromCharCode(code);
+}
+
+function randomNumber(): number {
+  return Math.floor(Math.random() * 10);
 }
