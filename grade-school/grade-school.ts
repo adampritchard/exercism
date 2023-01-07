f5a97d47659642e2026dd776c680932024684aa7
Adam Pritchard <hello@adampritchard.com.au>
2023-01-07 14:01:03 +1000

    grade-school; iteration 1

--- grade-school/grade-school.ts
+++ grade-school/grade-school.ts
@@ -1,13 +1,32 @@
+type Roster = Record<number, string[]>;
+
 export class GradeSchool {
-  roster() {
-    throw new Error('Remove this statement and implement this function')
+  protected _roster: Roster = {};
+
+  public roster(): Roster {
+    return structuredClone(this._roster);
   }
 
-  add() {
-    throw new Error('Remove this statement and implement this function')
+  public add(name: string, grade: number): void {
+    // If student is already in a grade, remove them.
+    for (const grade of Object.values(this._roster)) {
+      const index = grade.indexOf(name);
+      if (index !== -1) {
+        grade.splice(index, 1);
+      }
+    }
+
+    // Ensure an array exists for grade.
+    if (!this._roster[grade]) {
+      this._roster[grade] = [];
+    }
+
+    this._roster[grade].push(name);
+    this._roster[grade].sort();
   }
 
-  grade() {
-    throw new Error('Remove this statement and implement this function')
+  public grade(n: number): string[] {
+    const grade = this._roster[n] || [];
+    return structuredClone(grade);
   }
 }
