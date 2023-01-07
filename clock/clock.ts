f19db2964a86fa94ae77931695df773296c0b985
Adam Pritchard <hello@adampritchard.com.au>
2023-01-08 08:21:28 +1000

    clock; iteration 1

--- clock/clock.ts
+++ clock/clock.ts
@@ -1,21 +1,42 @@
 export class Clock {
-  constructor(hour: unknown, minute?: unknown) {
-    throw new Error('Remove this statement and implement this function')
+  public hours: number;
+  public minutes: number;
+
+  constructor(hours: number, minutes: number = 0) {
+    this.hours = hours;
+    this.minutes = minutes;
+    this.normalize();
   }
 
   public toString(): unknown {
-    throw new Error('Remove this statement and implement this function')
+    const hrs = this.hours.toFixed(0).padStart(2, '0');
+    const mins = this.minutes.toFixed(0).padStart(2, '0');
+    return `${hrs}:${mins}`;
   }
 
-  public plus(minutes: unknown): Clock {
-    throw new Error('Remove this statement and implement this function')
+  public plus(minutes: number): Clock {
+    return new Clock(this.hours, this.minutes + minutes);
   }
 
-  public minus(minutes: unknown): Clock {
-    throw new Error('Remove this statement and implement this function')
+  public minus(minutes: number): Clock {
+    return new Clock(this.hours, this.minutes - minutes);
   }
 
-  public equals(other: unknown): unknown {
-    throw new Error('Remove this statement and implement this function')
+  public equals(other: Clock): boolean {
+    return this.hours === other.hours
+        && this.minutes === other.minutes;
+  }
+
+  protected normalize(): void {
+    // Carry minutes to hours
+    this.hours += Math.floor(this.minutes / 60);
+
+    // Wrap hours and minutes
+    this.hours %= 24;
+    this.minutes %= 60;
+
+    // Convert negative hours and minutes
+    if (this.hours < 0) this.hours = 24 + this.hours;
+    if (this.minutes < 0) this.minutes = 60 + this.minutes;
   }
 }
