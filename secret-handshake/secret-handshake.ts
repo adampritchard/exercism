a43262f7206e785a276d677c180c5bef290fc77c
Adam Pritchard <hello@adampritchard.com.au>
2023-01-09 07:23:38 +1000

    secret-handshake; iteration 2

--- secret-handshake/secret-handshake.ts
+++ secret-handshake/secret-handshake.ts
@@ -1,32 +1,9 @@
 export function commands(n: number): string[] {
   const cmds: string[] = [];
-
-  const bits = toBits(n);
-  if (bits[0] === 1) cmds.push('wink');
-  if (bits[1] === 1) cmds.push('double blink');
-  if (bits[2] === 1) cmds.push('close your eyes');
-  if (bits[3] === 1) cmds.push('jump');
-  if (bits[4] === 1) cmds.reverse();
-
+  if (n & 0b00001) cmds.push('wink');
+  if (n & 0b00010) cmds.push('double blink');
+  if (n & 0b00100) cmds.push('close your eyes');
+  if (n & 0b01000) cmds.push('jump');
+  if (n & 0b10000) cmds.reverse();
   return cmds;
-}
-
-/**
- * Convert a number to an array of individual bits,
- * the bits are stored in least significant order. eg:
- *        LSB      MSB
- *   8 -> [0, 0, 0, 1]
- *  13 -> [1, 0, 1, 1]
- * 
- * @param n must be positive
- */
-function toBits(n: number): number[] {
-  const bits: number[] = [];
-
-  while (n > 0) {
-    bits.push(n % 2);
-    n = Math.floor(n / 2);
-  }
-
-  return bits;
 }
