5d59bd36fb0b594e731ed600fb11f764addf4dfd
Adam Pritchard <hello@adampritchard.com.au>
2023-01-09 17:17:02 +1000

    binary-search; iteration 1

--- binary-search/binary-search.ts
+++ binary-search/binary-search.ts
@@ -1,3 +1,13 @@
-export function find(haystack: unknown, needle: unknown): number | never {
-  throw new Error('Remove this statement and implement this function')
+export function find(haystack: number[], needle: number): number | never {
+  let min = 0;
+  let max = haystack.length - 1;
+
+  while (min <= max) {
+    const index = min + Math.floor((max - min) / 2);
+    if (haystack[index] === needle) return index;
+    if (haystack[index] > needle) max = index - 1;
+    if (haystack[index] < needle) min = index + 1;
+  }
+
+  throw new Error('Value not in array');
 }
