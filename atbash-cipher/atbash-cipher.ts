62918813fb9153612cebbea0bdb1ee99412c9d4b
Adam Pritchard <hello@adampritchard.com.au>
2023-01-12 08:14:27 +1000

    atbash-cipher; iteration 2

--- atbash-cipher/atbash-cipher.ts
+++ atbash-cipher/atbash-cipher.ts
@@ -1,14 +1,18 @@
+const plain  = 'abcdefghijklmnopqrstuvwxyz';
+const cipher = 'zyxwvutsrqponmlkjihgfedcba';
+
+const encodeMap = new Map<string, string>();
+const decodeMap = new Map<string, string>();
+for (let index = 0; index < plain.length; index++) {
+  encodeMap.set(plain[index], cipher[index]);
+  decodeMap.set(cipher[index], plain[index]);
+}
+
 export function encode(plainText: string): string {
-  const plainChars = plainText.toLowerCase().split('');
+  const plainChars = cleanChars(plainText);
+  const cipherChars = plainChars.map(char => encodeMap.get(char) ?? char);
 
-  // Convert plain chars to cipher chars.
-  const cipherChars = plainChars.reduce((arr, plainChar) => {
-    const cipherChar = encodeChar(plainChar);
-    if (cipherChar) arr.push(cipherChar);
-    return arr;
-  }, [] as string[]);
-
-  // Convert char array to string with spaces every 5 chars.
+  // Reduce char array to string with spaces every 5 chars.
   return cipherChars.reduce((str, char, index) => {
     if (index > 0 && index % 5 === 0) str += ' ';
     return str += char;
@@ -16,42 +20,11 @@ export function encode(plainText: string): string {
 }
 
 export function decode(cipherText: string): string {
-  const cipherChars = cipherText.split('');
-
-  // Convert cipher chars to plain chars.
-  const plainChars = cipherChars.reduce((arr, cipherChar) => {
-    const decoded = decodeChar(cipherChar);
-    if (decoded) arr.push(decoded);
-    return arr;
-  }, [] as string[]);
-
+  const cipherChars = cleanChars(cipherText);
+  const plainChars = cipherChars.map(char => decodeMap.get(char) ?? char);
   return plainChars.join('');
 }
 
-const plain  = 'abcdefghijklmnopqrstuvwxyz';
-const cipher = 'zyxwvutsrqponmlkjihgfedcba';
-
-const encodeMap = new Map<string, string>();
-const decodeMap = new Map<string, string>();
-
-for (let index = 0; index < plain.length; index++) {
-  encodeMap.set(plain[index], cipher[index]);
-  decodeMap.set(cipher[index], plain[index]);
-}
-
-function encodeChar(char: string): string|null {
-  return isNumberChar(char)
-    ? char
-    : encodeMap.get(char) ?? null;
-}
-
-function decodeChar(char: string): string|null {
-  return isNumberChar(char)
-    ? char
-    : decodeMap.get(char) ?? null;
-}
-
-function isNumberChar(char: string): boolean {
-  return char >= '0'
-      && char <= '9';
+function cleanChars(text: string): string[] {
+  return text.toLowerCase().replace(/[^\w]/g, '').split('');
 }
