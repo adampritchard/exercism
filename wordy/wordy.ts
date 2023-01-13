cece428722ee0116550550946a6e56b3eef1e173
Adam Pritchard <hello@adampritchard.com.au>
2023-01-13 18:44:49 +1000

    wordy; iteration 1

--- wordy/wordy.ts
+++ wordy/wordy.ts
@@ -1,3 +1,50 @@
-export const answer = () => {
-  throw new Error('Remove this statement and implement this function')
+export function answer(question: string): number {
+  const words = question.toLowerCase().replace('?', '').split(' ');
+
+  let result = 0;
+  while (words.length) {
+    const op = words.shift();
+    switch (op) {
+      case 'what':
+        requireText(words, 'is');
+        result = requireNumber(words);
+        break;
+
+      case 'plus':
+        result += requireNumber(words);
+        break;
+
+      case 'minus':
+        result -= requireNumber(words);
+        break;
+
+      case 'multiplied':
+        requireText(words, 'by');
+        result *= requireNumber(words);
+        break;
+
+      case 'divided':
+        requireText(words, 'by');
+        result /= requireNumber(words);
+        break;
+
+      default:
+        const msg = isNaN(Number(op)) ? 'Unknown operation' : 'Syntax error';
+        throw new Error(msg);
+    }
+  }
+
+  return result;
+}
+
+function requireNumber(words: string[]): number {
+  const n = Number(words.shift());
+  if (Number.isNaN(n)) throw new Error('Syntax error');
+  return n;
+}
+
+function requireText(words: string[], text: string): string {
+  const s = words.shift();
+  if (s !== text) throw new Error('Syntax error');
+  return s;
 }
