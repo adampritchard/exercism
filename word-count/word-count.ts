957eeddb5b79ea53e250aaaa719ba27a2ca0217d
Adam Pritchard <hello@adampritchard.com.au>
2023-01-14 20:40:13 +1000

    word-count; iteration 1

--- word-count/word-count.ts
+++ word-count/word-count.ts
@@ -1,3 +1,10 @@
-export function count() {
-  throw new Error('Remove this statement and implement this function')
+export function count(text: string): Map<string, number> {
+  const counts = new Map<string, number>();
+
+  const words = text.toLowerCase().trim().split(/\s+/);
+  for (const word of words) {
+    counts.set(word, 1 + (counts.get(word) ?? 0));
+  }
+
+  return counts;
 }
