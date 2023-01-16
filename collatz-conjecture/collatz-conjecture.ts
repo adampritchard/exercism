cd52d19db329aad4a90601c8246b29eab43bd551
Adam Pritchard <hello@adampritchard.com.au>
2023-01-17 06:33:07 +1000

    collatz-conjecture; iteration 1

--- collatz-conjecture/collatz-conjecture.ts
+++ collatz-conjecture/collatz-conjecture.ts
@@ -1,3 +1,11 @@
-export function steps(count: unknown): unknown {
-  throw new Error('Remove this statement and implement this function')
+export function steps(count: number): number {
+  if (count <= 0) {
+    throw new Error('Only positive numbers are allowed');
+  } else if (count === 1) {
+    return 0;
+  } else if (count % 2 === 0) {
+    return 1 + steps(count / 2);
+  } else {
+    return 1 + steps(count * 3 + 1);
+  }
 }
