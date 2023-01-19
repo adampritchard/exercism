0f6ed9062349506899843b52c87be0931eb7854d
Adam Pritchard <hello@adampritchard.com.au>
2023-01-20 06:12:14 +1000

    etl; iteration 1

--- etl/etl.ts
+++ etl/etl.ts
@@ -1,3 +1,11 @@
-export function transform(/* Parameters go here */) {
-  throw new Error('Remove this statement and implement this function')
+export function transform(legacyScoring: Record<number, string[]>): Record<string, number> {
+  const newScoring: Record<string, number> = {};
+
+  for (const [points, letters] of Object.entries(legacyScoring)) {
+    for (const letter of letters) {
+      newScoring[letter.toLowerCase()] = Number(points);
+    }
+  }
+
+  return newScoring;
 }
