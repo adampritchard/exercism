ac4d521a7fb1546f9802f80f461d43529f024d65
Adam Pritchard <hello@adampritchard.com.au>
2023-01-20 07:11:53 +1000

    protein-translation; iteration 1

--- protein-translation/protein-translation.ts
+++ protein-translation/protein-translation.ts
@@ -1,3 +1,37 @@
-export function translate() {
-  throw new Error('Remove this statement and implement this function')
+
+export function translate(rna: string): string[] {
+  const proteins: string[] = [];
+
+  const codons = rna.match(/\w\w\w/g) ?? [];
+  for (const codon of codons) {
+    const protein = getProtein(codon);
+    if (protein) {
+      proteins.push(protein);
+    } else {
+      break;
+    }
+  }
+
+  return proteins;
+}
+
+function getProtein(codon: string): string|null {
+  if (['AUG'].includes(codon)) return 'Methionine';
+
+  if (['UUU', 'UUC'].includes(codon)) return 'Phenylalanine';
+
+  if (['UUA', 'UUG'].includes(codon)) return 'Leucine';
+
+  if (['UCU', 'UCC', 'UCA', 'UCG'].includes(codon)) return 'Serine';
+
+  if (['UAU', 'UAC'].includes(codon)) return 'Tyrosine';
+
+  if (['UGU', 'UGC'].includes(codon)) return 'Cysteine';
+
+  if (['UGG'].includes(codon)) return 'Tryptophan';
+
+  // 'Stop' codons.
+  if (['UAA', 'UAG', 'UGA'].includes(codon)) return null;
+
+  return null;
 }
