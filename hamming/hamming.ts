b12eac3f63983d213a90eeef67256ea9bed9a99a
Adam Pritchard <hello@adampritchard.com.au>
2023-01-20 20:00:03 +1000

    hamming; iteration 1

--- hamming/hamming.ts
+++ hamming/hamming.ts
@@ -1,3 +1,12 @@
-export function compute(left: unknown, right: unknown): unknown {
-  throw new Error('Remove this statement and implement this function')
+export function compute(left: string, right: string): number {
+  if (left.length !== right.length) {
+    throw new Error('DNA strands must be of equal length.');
+  }
+
+  let distance = 0;
+  for (let i = 0; i < left.length; i += 1) {
+    if (left[i] !== right[i]) distance += 1;
+  }
+
+  return distance;
 }
