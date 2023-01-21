5a7b5a3c080563de05c2964ad0b543041adf8189
Adam Pritchard <hello@adampritchard.com.au>
2023-01-21 20:54:56 +1000

    allergies; iteration 2

--- allergies/allergies.ts
+++ allergies/allergies.ts
@@ -1,13 +1,29 @@
+const allergens = {
+  'eggs':           1,
+  'peanuts':        2,
+  'shellfish':      4,
+  'strawberries':   8,
+  'tomatoes':      16,
+  'chocolate':     32,
+  'pollen':        64,
+  'cats':         128,
+};
+
+type Allergen = keyof typeof allergens;
+
 export class Allergies {
-  constructor(allergenIndex: unknown) {
-    throw new Error('Remove this statement and implement this function')
+  public allergenIndex: number;
+
+  constructor(allergenIndex: number) {
+    this.allergenIndex = allergenIndex;
   }
 
-  public list(): unknown {
-    throw new Error('Remove this statement and implement this function')
+  public list(): Allergen[] {
+    const all = Object.keys(allergens) as Allergen[];
+    return all.filter(allergen => this.allergicTo(allergen));
   }
 
-  public allergicTo(allergen: unknown): unknown {
-    throw new Error('Remove this statement and implement this function')
+  public allergicTo(allergen: Allergen): boolean {
+    return !!(allergens[allergen] & this.allergenIndex);
   }
 }
