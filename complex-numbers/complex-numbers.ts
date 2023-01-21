58e4765380676ed305a42543b11f6c7597c156cd
Adam Pritchard <hello@adampritchard.com.au>
2023-01-22 07:36:09 +1000

    complex-numbers; iteration 2

--- complex-numbers/complex-numbers.ts
+++ complex-numbers/complex-numbers.ts
@@ -6,59 +6,42 @@ export class ComplexNumber {
 
   public add(other: ComplexNumber): ComplexNumber {
     // (a + i * b) + (c + i * d) = (a + c) + (b + d) * i
-
-    const a = this.real;
-    const b = this.imag;
-    const c = other.real;
-    const d = other.imag;
+    const [a, b, c, d] = [this.real, this.imag, other.real, other.imag];
     return new ComplexNumber(a + c, b + d);
   }
 
   public sub(other: ComplexNumber): ComplexNumber {
     // (a + i * b) - (c + i * d) = (a - c) + (b - d) * i
-
-    const a = this.real;
-    const b = this.imag;
-    const c = other.real;
-    const d = other.imag;
+    const [a, b, c, d] = [this.real, this.imag, other.real, other.imag];
     return new ComplexNumber(a - c, b - d);
   }
 
   public div(other: ComplexNumber): ComplexNumber {
     // (a + i * b) / (c + i * d) = (a * c + b * d)/(c^2 + d^2) + (b * c - a * d)/(c^2 + d^2) * i
-
-    const a = this.real;
-    const b = this.imag;
-    const c = other.real;
-    const d = other.imag;
-    return new ComplexNumber((a * c + b * d)/(c ** 2 + d ** 2), (b * c - a * d)/(c ** 2 + d ** 2));
+    const [a, b, c, d] = [this.real, this.imag, other.real, other.imag];
+    return new ComplexNumber((a * c + b * d) / (c ** 2 + d ** 2), (b * c - a * d) / (c ** 2 + d ** 2));
   }
 
   public mul(other: ComplexNumber): ComplexNumber {
     // (a + i * b) * (c + i * d) = (a * c - b * d) + (b * c + a * d) * i
-
-    const a = this.real;
-    const b = this.imag;
-    const c = other.real;
-    const d = other.imag;
+    const [a, b, c, d] = [this.real, this.imag, other.real, other.imag];
     return new ComplexNumber(a * c - b * d, b * c + a * d);
   }
 
   public get abs(): number {
     // |z| = sqrt(a^2 + b^2)
-    return Math.sqrt(this.real ** 2 + this.imag ** 2);
+    const [a, b] = [this.real, this.imag];
+    return Math.sqrt(a ** 2 + b ** 2);
   }
 
   public get conj(): ComplexNumber {
-    // TODO: is there a better way to avoid negative zero?
-    return new ComplexNumber(this.real, this.imag === 0 ? this.imag : -this.imag);
+    const [a, b] = [this.real, this.imag];
+    return new ComplexNumber(a, b ? -b : 0);
   }
 
   public get exp(): ComplexNumber {
     // e^(a + i * b) = e^a * cos(b) + i * sin(b)
-
-    const a = this.real;
-    const b = this.imag;
+    const [a, b] = [this.real, this.imag];
     return new ComplexNumber(Math.E ** a * Math.cos(b), Math.sin(b));
   }
 }
