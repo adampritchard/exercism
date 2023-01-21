36f944c02976a6e1ef3fccfe8e26548dfe816f9d
Adam Pritchard <hello@adampritchard.com.au>
2023-01-21 10:06:17 +1000

    nucleotide-count; iteration 1

--- nucleotide-count/nucleotide-count.ts
+++ nucleotide-count/nucleotide-count.ts
@@ -1,3 +1,22 @@
-export function nucleotideCounts(/* Parameters go here */) {
-  throw new Error('Remove this statement and implement this function')
+enum Nucleotide {
+  A = 'A',
+  C = 'C',
+  G = 'G',
+  T = 'T',
+};
+
+function isNucleotide(n: string): n is Nucleotide {
+  return Object.values(Nucleotide).includes(n as Nucleotide);
+}
+
+export function nucleotideCounts(dna: string): Record<Nucleotide, number> {
+  const counts: Record<Nucleotide, number> = { A: 0, C: 0, G: 0, T: 0 };
+
+  for (const n of dna) {
+    if (!isNucleotide(n)) throw new Error('Invalid nucleotide in strand');
+
+    counts[n] += 1;
+  }
+
+  return counts;
 }
