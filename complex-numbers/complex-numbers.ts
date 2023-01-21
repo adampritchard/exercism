cb079a7df0a2b71869ec975a5923e8bb1bf490f0
Adam Pritchard <hello@adampritchard.com.au>
2023-01-22 07:29:24 +1000

    complex-numbers; iteration 1

--- complex-numbers/complex-numbers.ts
+++ complex-numbers/complex-numbers.ts
@@ -1,41 +1,64 @@
 export class ComplexNumber {
-  constructor(real: unknown, imaginary: unknown) {
-    throw new Error('Remove this statement and implement this function')
+  constructor(
+    public readonly real: number,
+    public readonly imag: number,
+  ) {}
+
+  public add(other: ComplexNumber): ComplexNumber {
+    // (a + i * b) + (c + i * d) = (a + c) + (b + d) * i
+
+    const a = this.real;
+    const b = this.imag;
+    const c = other.real;
+    const d = other.imag;
+    return new ComplexNumber(a + c, b + d);
   }
 
-  public get real(): number {
-    throw new Error('Remove this statement and implement this function')
+  public sub(other: ComplexNumber): ComplexNumber {
+    // (a + i * b) - (c + i * d) = (a - c) + (b - d) * i
+
+    const a = this.real;
+    const b = this.imag;
+    const c = other.real;
+    const d = other.imag;
+    return new ComplexNumber(a - c, b - d);
   }
 
-  public get imag(): number {
-    throw new Error('Remove this statement and implement this function')
+  public div(other: ComplexNumber): ComplexNumber {
+    // (a + i * b) / (c + i * d) = (a * c + b * d)/(c^2 + d^2) + (b * c - a * d)/(c^2 + d^2) * i
+
+    const a = this.real;
+    const b = this.imag;
+    const c = other.real;
+    const d = other.imag;
+    return new ComplexNumber((a * c + b * d)/(c ** 2 + d ** 2), (b * c - a * d)/(c ** 2 + d ** 2));
   }
 
-  public add(other: unknown): unknown {
-    throw new Error('Remove this statement and implement this function')
+  public mul(other: ComplexNumber): ComplexNumber {
+    // (a + i * b) * (c + i * d) = (a * c - b * d) + (b * c + a * d) * i
+
+    const a = this.real;
+    const b = this.imag;
+    const c = other.real;
+    const d = other.imag;
+    return new ComplexNumber(a * c - b * d, b * c + a * d);
   }
 
-  public sub(other: unknown): unknown {
-    throw new Error('Remove this statement and implement this function')
+  public get abs(): number {
+    // |z| = sqrt(a^2 + b^2)
+    return Math.sqrt(this.real ** 2 + this.imag ** 2);
   }
 
-  public div(other: unknown): unknown {
-    throw new Error('Remove this statement and implement this function')
-  }
-
-  public mul(other: unknown): unknown {
-    throw new Error('Remove this statement and implement this function')
-  }
-
-  public get abs(): unknown {
-    throw new Error('Remove this statement and implement this function')
-  }
-
-  public get conj(): unknown {
-    throw new Error('Remove this statement and implement this function')
+  public get conj(): ComplexNumber {
+    // TODO: is there a better way to avoid negative zero?
+    return new ComplexNumber(this.real, this.imag === 0 ? this.imag : -this.imag);
   }
 
   public get exp(): ComplexNumber {
-    throw new Error('Remove this statement and implement this function')
+    // e^(a + i * b) = e^a * cos(b) + i * sin(b)
+
+    const a = this.real;
+    const b = this.imag;
+    return new ComplexNumber(Math.E ** a * Math.cos(b), Math.sin(b));
   }
 }
