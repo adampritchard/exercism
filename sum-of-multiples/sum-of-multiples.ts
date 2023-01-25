3d02106786b30daedde8f895c5176d607597f856
Adam Pritchard <hello@adampritchard.com.au>
2023-01-26 08:15:33 +1000

    sum-of-multiples; iteration 2

--- sum-of-multiples/sum-of-multiples.ts
+++ sum-of-multiples/sum-of-multiples.ts
@@ -1,14 +1,15 @@
 export function sum(numbers: number[], max: number): number {
-  const multiples = new Set<number>();
+  const multiples: number[] = [];
 
   for (let i = 1; i < max; i += 1) {
     for (const n of numbers) {
       if (i % n === 0) {
-        multiples.add(i);
+        multiples.push(i);
+        break;
       }
     }
   }
-  
+
   // Return the sum of the multiples.
-  return Array.from(multiples).reduce((acc, val) => acc + val, 0);
+  return multiples.reduce((acc, val) => acc + val, 0);
 }
