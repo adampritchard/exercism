01a7fa3cb9c7daaf783cb732ff5c1959c20922ad
Adam Pritchard <hello@adampritchard.com.au>
2023-01-26 16:35:44 +1000

    acronym; iteration 1

--- acronym/acronym.ts
+++ acronym/acronym.ts
@@ -1,3 +1,13 @@
-export function parse(phrase: unknown): unknown {
-  throw new Error('Remove this statement and implement this function')
+export function parse(phrase: string): string {
+  return Array.from(phrase)
+    .filter((char, idx) =>
+      // First letter.
+      idx === 0
+      // Prev letter is a space or dash.
+      || phrase[idx - 1].match(/[\s\-]/)
+      // Letter is a capital, and the prev letter is not.
+      || phrase[idx].match(/[A-Z]/) && phrase[idx - 1].match(/[a-z]/)
+    )
+    .join('')
+    .toUpperCase();
 }
