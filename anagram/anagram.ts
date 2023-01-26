68919b176c94447bcc2f06b9a68dd0474fd331a0
Adam Pritchard <hello@adampritchard.com.au>
2023-01-26 17:14:00 +1000

    anagram; iteration 2

--- anagram/anagram.ts
+++ anagram/anagram.ts
@@ -4,25 +4,13 @@ export class Anagram {
   ) {}
 
   public matches(...potentials: string[]): string[] {
-    const inputLetters = this.getLetterCounts(this.input);
+    const lowCaseInput = this.input.toLowerCase();
+    const sortedInput = [...lowCaseInput].sort().join('');
 
     return potentials.filter(word => {
-      if (this.input.length !== word.length) return false;
-      if (this.input.toLowerCase() === word.toLowerCase()) return false;
-
-      return Object
-        .entries(this.getLetterCounts(word))
-        .every(([letter, count]) => inputLetters[letter] === count);
+      const lowCaseWord = word.toLowerCase();
+      return lowCaseInput !== lowCaseWord
+        && sortedInput === [...lowCaseWord].sort().join('');
     });
-  }
-
-  protected getLetterCounts(word: string): Record<string, number> {
-    const counts: Record<string, number> = {};
-
-    for (const char of word.toLowerCase()) {
-      counts[char] = 1 + (counts[char] || 0);
-    }
-
-    return counts;
   }
 }
