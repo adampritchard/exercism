bb46594f711f793de0544a36ad32e98b7e903458
Adam Pritchard <hello@adampritchard.com.au>
2023-01-31 06:40:04 +1000

    isogram; iteration 1

--- isogram/isogram.ts
+++ isogram/isogram.ts
@@ -1,3 +1,17 @@
-export function isIsogram(/* parameters go here */): unknown {
-  throw new Error('Remove this statement and implement this function')
+export function isIsogram(text: string): boolean {
+  // Remove whitespace and hyphens.
+  text = text.replace(/[\s-]/g, '').toLowerCase();
+
+  const found: Record<string, boolean> = {};
+  for (const char of text) {
+    if (found[char]) {
+      // Found a repeated letter.
+      return false;
+    }
+
+    found[char] = true;
+  }
+
+  // Found no repeated letters.
+  return true;
 }
