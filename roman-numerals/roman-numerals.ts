b09b8125a6b861b821261c9ffaf34808374680b3
Adam Pritchard <hello@adampritchard.com.au>
2023-02-10 07:18:08 +1000

    roman-numerals; iteration 1

--- roman-numerals/roman-numerals.ts
+++ roman-numerals/roman-numerals.ts
@@ -1,3 +1,28 @@
-export const toRoman = () => {
-  throw new Error('Remove this statement and implement this function')
+const map = [
+  { value: 1000, numeral: 'M' },
+  { value:  900, numeral: 'CM' },
+  { value:  500, numeral: 'D' },
+  { value:  400, numeral: 'CD' },
+  { value:  100, numeral: 'C' },
+  { value:   90, numeral: 'XC' },
+  { value:   50, numeral: 'L' },
+  { value:   40, numeral: 'XL' },
+  { value:   10, numeral: 'X' },
+  { value:    9, numeral: 'IX' },
+  { value:    5, numeral: 'V' },
+  { value:    4, numeral: 'IV' },
+  { value:    1, numeral: 'I' },
+];
+
+export const toRoman = (n: number): string => {
+  let numerals = '';
+
+  for (const { value, numeral } of map) {
+    while (n >= value) {
+      numerals += numeral;
+      n -= value;
+    }
+  }
+
+  return numerals;
 }
