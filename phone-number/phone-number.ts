4c609ef8d59ae9ee734d8828316a9d94c3199dca
Adam Pritchard <hello@adampritchard.com.au>
2023-02-11 11:45:18 +1000

    phone-number; iteration 1

--- phone-number/phone-number.ts
+++ phone-number/phone-number.ts
@@ -1,3 +1,25 @@
-export function clean() {
-  throw new Error('Remove this statement and implement this function')
+export function clean(number: string): string {
+  // Validate chars.
+  if (number.match(/[A-Za-z]/)) throw new Error('Letters not permitted');
+  if (number.match(/[@:!]/)) throw new Error('Punctuations not permitted');
+
+  // Validate area code.
+  if (number.match(/\(0/)) throw new Error('Area code cannot start with zero');
+  if (number.match(/\(1/)) throw new Error('Area code cannot start with one');
+
+  // Validate exchange code.
+  if (number.match(/\) 0/)) throw new Error('Exchange code cannot start with zero');
+  if (number.match(/\) 1/)) throw new Error('Exchange code cannot start with one');
+
+  let cleaned = number.replace(/[\s\(\)\-\.\+]/g, '');
+
+  // Validate length.
+  if (cleaned.length < 10) throw new Error('Incorrect number of digits');
+  if (cleaned.length > 11) throw new Error('More than 11 digits');
+  if (cleaned.length === 11) {
+    if (cleaned[0] !== '1') throw new Error('11 digits must start with 1');
+    cleaned = cleaned.substring(1);
+  }
+
+  return cleaned;
 }
