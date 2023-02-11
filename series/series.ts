5e577d655f3276e1393ba893ba130897f7ba832a
Adam Pritchard <hello@adampritchard.com.au>
2023-02-11 11:04:30 +1000

    series; iteration 1

--- series/series.ts
+++ series/series.ts
@@ -1,9 +1,21 @@
 export class Series {
-  constructor(series: unknown) {
-    throw new Error('Remove this statement and implement this function')
+  protected numbers: number[];
+
+  constructor(series: string) {
+    if (series === '') throw new Error('series cannot be empty');
+    this.numbers = series.split('').map(Number);
   }
 
-  slices(sliceLength: unknown): unknown {
-    throw new Error('Remove this statement and implement this function')
+  slices(sliceLength: number): number[][] {
+    if (sliceLength === 0) throw new Error('slice length cannot be zero');
+    if (sliceLength < 0) throw new Error('slice length cannot be negative');
+    if (sliceLength > this.numbers.length) throw new Error('slice length cannot be greater than series length');
+
+    const result = [];
+    for (let i = 0; i + sliceLength <= this.numbers.length; i += 1) {
+      result.push(this.numbers.slice(i, sliceLength + i));
+    }
+
+    return result;
   }
 }
