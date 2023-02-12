d3339e94385489d80c1ad2fc1d0426a706f367a4
Adam Pritchard <hello@adampritchard.com.au>
2023-02-13 08:02:43 +1000

    two-bucket; iteration 2

--- two-bucket/two-bucket.ts
+++ two-bucket/two-bucket.ts
@@ -1,109 +1,85 @@
-type BucketId = 'one' | 'two';
+type BucketName = 'one' | 'two';
+
+type Bucket = {
+  name: BucketName,
+  size: number,
+  fill: number,
+};
 
 export class TwoBucket {
-  protected b1: number;
-  protected b2: number;
+  protected a: Bucket;
+  protected b: Bucket;
+  protected goal: number;
 
   constructor(
-    protected size1: number,
-    protected size2: number,
-    protected goal: number,
-    protected start: BucketId,
+    size1: number,
+    size2: number,
+    goal: number,
+    start: BucketName,
   ) {
-    this.b1 = 0;
-    this.b2 = 0;
+    const b1 = { name: 'one' as const, size: size1, fill: 0 };
+    const b2 = { name: 'two' as const, size: size2, fill: 0 };
+
+    this.a = start === 'one' ? b1 : b2;
+    this.b = start === 'one' ? b2 : b1;
+
+    this.goal = goal;
   }
 
   moves(): number {
-    if (this.goal > this.size1 && this.goal > this.size2) {
+    if (this.goal > this.a.size && this.goal > this.b.size) {
       throw new Error('Goal unreachable');
     }
 
     let moveCount = 0;
 
-    if (this.start === 'one') {
+    // Check if we can just fill bucket b and be done.
+    if (this.b.size === this.goal) {
+      this.a.fill = this.a.size;
+      this.b.fill = this.b.size;
+      return 2;
+    }
 
-      // Check if we can just fill the other bucket and be done.
-      if (this.size2 === this.goal) {
-        this.b1 = this.size1;
-        this.b2 = this.size2;
-        return 2;
-      }
-
-
-      while (this.b1 !== this.goal && this.b2 !== this.goal) {
-        if (this.b2 === this.size2) {
-          // empty b2.
-          this.b2 = 0;
-        } else {
-          // fill b1.
-          this.b1 = this.size1;
-        }
-        moveCount += 1;
-
-        if (this.b1 === this.goal || this.b2 === this.goal) {
-          break;
-        }
-
-        // pour b1 into b2.
-        const moved = Math.min(this.b1, this.size2 - this.b2);
-        this.b2 += moved;
-        this.b1 -= moved;
-        moveCount += 1;
-
-        // temp
-        if (moveCount >= 100) {
-          throw new Error('Goal unreachable');
-        }
-      }
-    } else {
-      // Check if we can just fill the other bucket and be done.
-      if (this.size1 === this.goal) {
-        this.b1 = this.size1;
-        this.b2 = this.size2;
-        return 2;
-      }
-
-      while (this.b1 !== this.goal && this.b2 !== this.goal) {
-        if (this.b1 === this.size1) {
-          // empty b1.
-          this.b1 = 0;
-        } else {
-          // fill b2.
-          this.b2 = this.size2;
-        }
-        moveCount += 1;
-
-        if (this.b1 === this.goal || this.b2 === this.goal) {
-          break;
-        }
-
-        // pour b2 into b1.
-        const moved = Math.min(this.b2, this.size1 - this.b1);
-        this.b1 += moved;
-        this.b2 -= moved;
-        moveCount += 1;
-      }
-
-      // temp
-      if (moveCount >= 100) {
+    while (true) {
+      // Check if we're just back to where we started.
+      if (moveCount > 0 && this.a.fill === 0 && this.b.fill === 0) {
         throw new Error('Goal unreachable');
       }
+
+      if (this.b.fill === this.b.size) {
+        // empty b.
+        this.b.fill = 0;
+      } else {
+        // fill a.
+        this.a.fill = this.a.size;
+      }
+      moveCount += 1;
+      if (this.hasMetGoal()) break;
+
+      // pour a into b.
+      const moved = Math.min(this.a.fill, this.b.size - this.b.fill);
+      this.b.fill += moved;
+      this.a.fill -= moved;
+      moveCount += 1;
+      if (this.hasMetGoal()) break;
     }
 
     return moveCount;
   }
 
-  get goalBucket(): BucketId {
-    if (this.b1 === this.goal) return 'one';
-    if (this.b2 === this.goal) return 'two';
-    throw new Error('No goal bucket!');
+  protected hasMetGoal(): boolean {
+    return this.a.fill === this.goal || this.b.fill === this.goal;
+  }
+
+  get goalBucket(): BucketName {
+    return this.a.fill === this.goal
+      ? this.a.name
+      : this.b.name;
   }
 
   get otherBucket(): number {
-    switch (this.goalBucket) {
-      case 'one': return this.b2;
-      case 'two': return this.b1;
-    }
+    return this.a.fill === this.goal
+      ? this.b.fill
+      : this.a.fill;
   }
 }
