a9154651222ffc67c306085aa705129b8b17e49b
Adam Pritchard <hello@adampritchard.com.au>
2023-02-19 18:00:04 +1000

    largest-series-product; iteration 1

--- largest-series-product/largest-series-product.ts
+++ largest-series-product/largest-series-product.ts
@@ -1,3 +1,27 @@
-export const largestProduct = () => {
-  throw new Error('Remove this statement and implement this function')
+export const largestProduct = (digitString: string, span: number): number => {
+  if (span > digitString.length) {
+    throw new Error('Span must be smaller than string length');
+  } else if (span < 0) {
+    throw new Error('Span must not be negative');
+  } else if (digitString.match(/[^\d]/)) {
+    throw new Error('Digits input must only contain digits');
+  }
+
+  const digits: number[] = digitString.split('').map(Number);
+
+  let largest = 0;
+  for (let i = 0; i <= digits.length - span; i += 1) {
+    largest = Math.max(largest, product(digits, i, i + span));
+  }
+
+  return largest;
+}
+
+function product(values: number[], rangeStart: number, rangeEnd: number): number {
+  let product = 1;
+  for (let i = rangeStart; i < Math.min(rangeEnd, values.length); i += 1) {
+    product *= values[i];
+  }
+
+  return product;
 }
