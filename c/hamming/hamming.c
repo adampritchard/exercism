1cf4b8c228ca58066f44e8655ff6aee667ab7781
Adam Pritchard <hello@adampritchard.com.au>
2023-02-26 16:58:51 +1000

    c; hamming; iteration 1

--- c/hamming/hamming.c
+++ c/hamming/hamming.c
@@ -1,1 +1,18 @@
 #include "hamming.h"
+
+int compute(const char *lhs, const char *rhs)
+{
+    int dist = 0;
+
+    // iterate over both pointers.
+    for (; *lhs && *rhs; lhs += 1, rhs += 1)
+    {
+        // increase distance if not equal.
+        if (*lhs != *rhs) dist += 1;
+    }
+
+    // invalid if lhs & rhs are not same length.
+    if (*lhs != '\0' || *rhs != '\0') return -1;
+
+    return dist;
+}
