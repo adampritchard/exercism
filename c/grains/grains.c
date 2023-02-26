bddcb45c136a0428c5c38177d2daf15d7865935f
Adam Pritchard <hello@adampritchard.com.au>
2023-02-26 18:11:13 +1000

    c; grains; iteration 2

--- c/grains/grains.c
+++ c/grains/grains.c
@@ -3,8 +3,7 @@
 uint64_t square(uint8_t index)
 {
     if (index < 1 || index > 64) return 0;
-    if (index == 1) return 1;
-    return 2 * square(index - 1);
+    return (uint64_t)1 << (index - 1);
 }
 
 uint64_t total()
