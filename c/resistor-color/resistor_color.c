d2c34d30f565b290639eead0e56b5152fd4c38d8
Adam Pritchard <hello@adampritchard.com.au>
2023-02-26 15:45:17 +1000

    c; resistor-color; iteration 1

--- c/resistor-color/resistor_color.c
+++ c/resistor-color/resistor_color.c
@@ -1,1 +1,24 @@
 #include "resistor_color.h"
+
+int color_code(resistor_band_t band)
+{
+    return (int)band;
+}
+
+const resistor_band_t* colors()
+{
+    static const resistor_band_t arr[] = {
+        BLACK,
+        BROWN,
+        RED,
+        ORANGE,
+        YELLOW,
+        GREEN,
+        BLUE,
+        VIOLET,
+        GREY,
+        WHITE,
+    };
+
+    return arr;
+}
