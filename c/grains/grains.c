ca01a51cd61b302677748dd5efff95d8bb645e48
Adam Pritchard <hello@adampritchard.com.au>
2023-02-26 18:03:09 +1000

    c; grains; iteration 1

--- c/grains/grains.c
+++ c/grains/grains.c
@@ -1,1 +1,18 @@
 #include "grains.h"
+
+uint64_t square(uint8_t index)
+{
+    if (index < 1 || index > 64) return 0;
+    if (index == 1) return 1;
+    return 2 * square(index - 1);
+}
+
+uint64_t total()
+{
+    uint64_t sum = 0;
+    for (int index = 1; index <= 64; index += 1)
+    {
+        sum += square(index);
+    }
+    return sum;
+}
