38a1bd177c67d90cd61bd8b99dffa330d5a69076
Adam Pritchard <hello@adampritchard.com.au>
2023-02-27 19:27:44 +1000

    c; difference-of-squares; iteration 2

--- c/difference-of-squares/difference_of_squares.c
+++ c/difference-of-squares/difference_of_squares.c
@@ -1,28 +1,17 @@
 #include "difference_of_squares.h"
 
-unsigned int sum_of_squares(unsigned int number)
+unsigned int sum_of_squares(unsigned int n)
 {
-    unsigned int sum = 0;
-    while (number) {
-        sum += number * number;
-        number -= 1;
-    }
-
-    return sum;
+    return n * (n + 1) * (2 * n + 1) / 6;
 }
 
-unsigned int square_of_sum(unsigned int number)
+unsigned int square_of_sum(unsigned int n)
 {
-    unsigned int sum = 0;
-    while (number) {
-        sum += number;
-        number -= 1;
-    }
-
+    unsigned int sum = n * (n + 1) / 2;
     return sum * sum;
 }
 
-unsigned int difference_of_squares(unsigned int number)
+unsigned int difference_of_squares(unsigned int n)
 {
-    return square_of_sum(number) - sum_of_squares(number);
+    return square_of_sum(n) - sum_of_squares(n);
 }
