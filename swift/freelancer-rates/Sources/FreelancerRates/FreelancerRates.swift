a93129444394d9fd05a34e3e4f0feb49db72d990
Adam Pritchard <hello@adampritchard.com.au>
2023-09-22 06:48:49 +1000

    swift; freelancer-rates; iteration 2

--- swift/freelancer-rates/Sources/FreelancerRates/FreelancerRates.swift
+++ swift/freelancer-rates/Sources/FreelancerRates/FreelancerRates.swift
@@ -1,13 +1,22 @@
+let workHoursInDay = 8.0
+let workDaysInMonth = 22.0
+
 func dailyRateFrom(hourlyRate: Int) -> Double {
-  Double(hourlyRate) * 8.0
+  Double(hourlyRate) * workHoursInDay
 }
 
 func monthlyRateFrom(hourlyRate: Int, withDiscount discount: Double) -> Double {
-  let monthlyRate = dailyRateFrom(hourlyRate: hourlyRate) * 22.0 * (100.0 - discount) / 100.0
-	return monthlyRate.rounded()
+  let monthlyRate = dailyRateFrom(hourlyRate: hourlyRate) * workDaysInMonth
+	let discountedRate = applyDiscount(discount, toRate: monthlyRate)
+	return discountedRate.rounded()
 }
 
 func workdaysIn(budget: Double, hourlyRate: Int, withDiscount discount: Double) -> Double {
-  let dailyRate = dailyRateFrom(hourlyRate: hourlyRate) * (100.0 - discount) / 100.0
-	return (budget / dailyRate).rounded(.down)
+	let dailyRate = dailyRateFrom(hourlyRate: hourlyRate)
+  let discountedRate = applyDiscount(discount, toRate: dailyRate)
+	return (budget / discountedRate).rounded(.down)
+}
+
+func applyDiscount(_ discount: Double, toRate rate: Double) -> Double {
+	rate * (1.0 - discount * 0.01)
 }
