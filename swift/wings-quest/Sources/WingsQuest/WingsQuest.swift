0a3e402596e735e2154a77432d61fecc6b0deb2b
Adam Pritchard <hello@adampritchard.com.au>
2023-09-21 21:36:17 +1000

    swift; wings-quest; iteration 1

--- swift/wings-quest/Sources/WingsQuest/WingsQuest.swift
+++ swift/wings-quest/Sources/WingsQuest/WingsQuest.swift
@@ -1,15 +1,15 @@
 func bonusPoints(powerUpActive: Bool, touchingEagle: Bool) -> Bool {
-  fatalError("Please implement the bonusPoints(powerUpActive:touchingEagle:) function")
+  powerUpActive && touchingEagle
 }
 
 func score(touchingPowerUp: Bool, touchingSeed: Bool) -> Bool {
-  fatalError("Please implement the score(touchingPowerUp:touchingSeed:) function")
+  touchingPowerUp || touchingSeed
 }
 
 func lose(powerUpActive: Bool, touchingEagle: Bool) -> Bool {
-  fatalError("Please implement the lose(powerUpActive:touchingEagle:) function")
+  !powerUpActive && touchingEagle
 }
 
 func win(hasPickedUpAllSeeds: Bool, powerUpActive: Bool, touchingEagle: Bool) -> Bool {
-  fatalError("Please implement the win(hasPickedUpAllSeeds:powerUpActive:touchingEagle:) function")
+  hasPickedUpAllSeeds && !lose(powerUpActive: powerUpActive, touchingEagle: touchingEagle)
 }
