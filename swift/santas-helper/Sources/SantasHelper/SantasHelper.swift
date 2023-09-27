fe4daa5aa3b305c5b00617dd6e46a91a3a19e3f4
Adam Pritchard <hello@adampritchard.com.au>
2023-09-27 19:00:19 +1000

    swift; santas-helper; iteration 1

--- swift/santas-helper/Sources/SantasHelper/SantasHelper.swift
+++ swift/santas-helper/Sources/SantasHelper/SantasHelper.swift
@@ -1,12 +1,21 @@
 import Foundation
 
 func cartesianToPolar(_ cart: (x: Double, y: Double)) -> (r: Double, phi: Double) {
-  fatalError("Please implement the cartesianToPolar(_:) function.")
+  let (x, y) = cart
+  return (
+    r: sqrt(pow(x, 2) + pow(y, 2)),
+    phi: atan2(y, x)
+  )
 }
 
 func combineRecords(
   production: (toy: String, id: Int, productLead: String),
   gifts: (Int, [String])
 ) -> (id: Int, toy: String, productLead: String, recipients: [String]) {
-  fatalError("Please implement the combineRecords(production:gifts:) function.")
+  return (
+    id: production.id,
+    toy: production.toy,
+    productLead: production.productLead,
+    recipients: gifts.1
+  )
 }
