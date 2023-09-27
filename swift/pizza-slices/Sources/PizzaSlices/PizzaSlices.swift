c1bba43c69cdb203021247cbbd6cb4f73556b910
Adam Pritchard <hello@adampritchard.com.au>
2023-09-27 19:26:41 +1000

    swift; pizza-slices; iteration 1

--- swift/pizza-slices/Sources/PizzaSlices/PizzaSlices.swift
+++ swift/pizza-slices/Sources/PizzaSlices/PizzaSlices.swift
@@ -1,10 +1,40 @@
+import Foundation
+
 func sliceSize(diameter: Double?, slices: Int?) -> Double? {
-  fatalError("Please implement the sliceSize(diameter:slices:) function")
+  guard let diameter = diameter, let slices = slices else {
+    return nil
+  }
+
+  guard slices > 0 && diameter >= 0 else {
+    return nil
+  }
+
+  let r = diameter / 2
+  return Double.pi * r * r / Double(slices)
 }
 
 func biggestSlice(
   diameterA: String, slicesA: String,
   diameterB: String, slicesB: String
 ) -> String {
-  fatalError("Please implement the biggestSlice(diameterA:slicesA:diameterB:slicesB:) function")
+  let sizeA = sliceSize(diameter: Double(diameterA), slices: Int(slicesA))
+  let sizeB = sliceSize(diameter: Double(diameterB), slices: Int(slicesB))
+
+  if sizeA == sizeB {
+    return "Neither slice is bigger"
+  }
+
+  guard let sizeA = sizeA else {
+    return "Slice B is bigger"
+  }
+
+  guard let sizeB = sizeB else {
+    return "Slice A is bigger"
+  }
+
+  if sizeA > sizeB {
+    return "Slice A is bigger"
+  } else {
+    return "Slice B is bigger"
+  }
 }
