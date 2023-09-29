3acf66f0e91a35984737a3be21df65479dc4435b
Adam Pritchard <hello@adampritchard.com.au>
2023-09-29 21:11:35 +1000

    swift; master-mixologist; iteration 2

--- swift/master-mixologist/Sources/MasterMixologist/MasterMixologist.swift
+++ swift/master-mixologist/Sources/MasterMixologist/MasterMixologist.swift
@@ -1,26 +1,22 @@
-func timeToPrepare(drink: String) -> Double {
-  switch drink {
-    case "beer", "soda", "water": return 0.5
-    case "shot": return 1.0
-    case "mixed drink": return 1.5
-    case "fancy drink": return 2.5
-    case "frozen drink": return 3.0
-    default: fatalError("unknown drink: '\(drink)'")
-  }
-}
+let prepTimes = [
+  "beer":         0.5,
+  "soda":         0.5,
+  "water":        0.5,
+  "shot":         1.0,
+  "mixed drink":  1.5,
+  "fancy drink":  2.5,
+  "frozen drink": 3.0,
+]
+
+let wedgeCounts = [
+  "small":   6,
+  "medium":  8,
+  "large":  10,
+]
 
 func timeToPrepare(drinks: [String]) -> Double {
-  drinks.reduce(0.0) { time, drink in
-    time + timeToPrepare(drink: drink)
-  }
-}
-
-func wedgeCount(forSize size: String) -> Int {
-  switch size {
-    case "small":  return 6
-    case "medium": return 8
-    case "large":  return 10
-    default: fatalError("unknown size \(size)")
+  drinks.reduce(0) { time, drink in
+    time + prepTimes[drink, default: 0]
   }
 }
 
@@ -28,12 +24,9 @@ func makeWedges(needed: Int, limes: [String]) -> Int {
   var limesUsed = 0
   var stillNeeded = needed
 
-  while stillNeeded > 0 {
-    guard limes.count > limesUsed else {
-      break
-    }
-
-    stillNeeded -= wedgeCount(forSize: limes[limesUsed])
+  while stillNeeded > 0, limes.count > limesUsed {
+    let size = limes[limesUsed]
+    stillNeeded -= wedgeCounts[size, default: 0]
     limesUsed += 1
   }
 
@@ -44,37 +37,27 @@ func finishShift(minutesLeft: Int, remainingOrders: [[String]]) -> [[String]] {
   var minutes = Double(minutesLeft)
   var orders = remainingOrders
 
-  while minutes > 0, orders.count > 0 {
-    let order = orders.remove(at: 0)
+  while minutes > 0, !orders.isEmpty {
+    let order = orders.removeFirst()
     minutes -= timeToPrepare(drinks: order)
   }
 
   return orders
 }
 
-typealias Tracking = (first: String, last: String, total: Int)
+typealias OrderTimes = (first: String, last: String, total: Int)
 
-func orderTracker(orders: [(drink: String, time: String)]) -> (beer: Tracking?, soda: Tracking?) {
-  var beer: Tracking? = nil
-  var soda: Tracking? = nil
+func orderTracker(orders: [(drink: String, time: String)]) -> (beer: OrderTimes?, soda: OrderTimes?) {
+  var drinks: [String: OrderTimes] = [:]
 
   for order in orders {
-    if order.drink == "beer" {
-      beer = (
-        first: beer?.first ?? order.time,
-        last: order.time,
-        total: (beer?.total ?? 0) + 1
-      )
-    }
-
-    if order.drink == "soda" {
-      soda = (
-        first: soda?.first ?? order.time,
-        last: order.time,
-        total: (soda?.total ?? 0) + 1
-      )
-    }
+    let drink = drinks[order.drink]
+    drinks[order.drink] = (
+      first: drink?.first ?? order.time,
+      last: order.time,
+      total: (drink?.total ?? 0) + 1
+    )
   }
 
-  return (beer: beer, soda: soda)
+  return (beer: drinks["beer"], soda: drinks["soda"])
 }
