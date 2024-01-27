e3c410cdcd433804e1e350b13cbc8fdc613610bc
Adam Pritchard <hello@adampritchard.com.au>
2024-01-27 11:12:58 +1000

    typescript; isbn-verifier; iteration 2

--- typescript/isbn-verifier/isbn-verifier.ts
+++ typescript/isbn-verifier/isbn-verifier.ts
@@ -3,21 +3,10 @@ export function isValid(isbn: string): boolean {
 
   if (isbn.length !== 10) return false;
 
-  let acc = 0;
-  for (let i = 0; i < 10; i += 1) {
-    const c = isbn.charAt(i);
-
-    const n = (i === 9 && c === 'X') ? 10 : Number(c);
-
-    if (Number.isNaN(n)) {
-      return false;
-    }
-
-    acc += n * (10 - i);
-  }
+  const acc = Array.from(isbn).reduce<number>((acc, char, index) => {
+    const n = (index === 9 && char === 'X') ? 10 : Number(char);
+    return acc + (n * (10 - index));
+  }, 0);
 
   return acc % 11 === 0;
 }
-
-
-// (d₁ * 10 + d₂ * 9 + d₃ * 8 + d₄ * 7 + d₅ * 6 + d₆ * 5 + d₇ * 4 + d₈ * 3 + d₉ * 2 + d₁₀ * 1) mod 11 == 0
