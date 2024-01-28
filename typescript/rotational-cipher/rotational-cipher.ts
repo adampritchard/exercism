b77cde230964b1e571d7154df5f5f935966be53b
Adam Pritchard <hello@adampritchard.com.au>
2024-01-28 15:17:44 +1000

    typescript; rotational-cipher; iteration 1

--- typescript/rotational-cipher/rotational-cipher.ts
+++ typescript/rotational-cipher/rotational-cipher.ts
@@ -1,3 +1,24 @@
-export function rotate() {
-  throw new Error('Remove this statement and implement this function')
+export function rotate(text: string, offset: number): string {
+  return mapString(text, char => offsetChar(char, offset));
+}
+
+function offsetChar(char: string, offset: number): string {
+  if (!char.match(/[A-Za-z]/)) {
+    return char;
+  }
+
+  const charCode = char.charCodeAt(0);
+
+  const aChar = char.match(/[A-Z]/) ? 'A' : 'a';
+  const aCharCode = aChar.charCodeAt(0);
+
+  const offsetCharCode = ((charCode + offset - aCharCode) % 26) + aCharCode;
+  return String.fromCharCode(offsetCharCode);
+}
+
+function mapString(text: string, fn: (s: string) => string): string {
+  return Array
+    .from(text)
+    .map(fn)
+    .join('');
 }
