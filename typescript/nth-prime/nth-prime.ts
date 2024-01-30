28ee7306a5d331bf9637f583f45d6946701579d8
Adam Pritchard <hello@adampritchard.com.au>
2024-01-31 07:10:24 +1000

    typescript; nth-prime; iteration 1

--- typescript/nth-prime/nth-prime.ts
+++ typescript/nth-prime/nth-prime.ts
@@ -1,3 +1,33 @@
-export function nth() {
-  throw new Error('Remove this statement and implement this function')
+export function nth(n: number): number {
+  if (n === 0) {
+    throw new Error('Prime is not possible');
+  }
+
+  let prime = 0;
+
+  let i = 1;
+  while (n) {
+    if (isPrime(i)) {
+      prime = i;
+      n -= 1;
+    }
+
+    i += 1;
+  }
+
+  return prime;
+}
+
+function isPrime(n: number): boolean {
+  if (n < 2) {
+    return false;
+  }
+
+  for (let i = 2; i <= (n / 2); i += 1) {
+    if (n % i === 0) {
+      return false;
+    }
+  }
+  
+  return true;
 }
