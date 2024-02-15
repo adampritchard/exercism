8068822eb9d3af3087816da1a88860ba690220e0
Adam Pritchard <hello@adampritchard.com.au>
2024-02-15 19:08:22 +1000

    typescript: custom-set; iteration 1

--- typescript/custom-set/custom-set.ts
+++ typescript/custom-set/custom-set.ts
@@ -1,41 +1,97 @@
 export class CustomSet {
-  constructor(initial?: unknown) {
-    throw new Error('Remove this statement and implement this function')
+  protected values: number[];
+
+  constructor(initial?: number[]) {
+    this.values = [];
+
+    if (initial) {
+      for (const val of initial) {
+        this.add(val);
+      }
+    }
   }
 
-  empty(): unknown {
-    throw new Error('Remove this statement and implement this function')
+  empty(): boolean {
+    return this.values.length === 0;
   }
 
-  contains(element: unknown): unknown {
-    throw new Error('Remove this statement and implement this function')
+  contains(element: number): boolean {
+    for (const val of this.values) {
+      if (val === element) {
+        return true;
+      }
+    }
+
+    return false;
   }
 
-  add(element: unknown): CustomSet {
-    throw new Error('Remove this statement and implement this function')
+  add(element: number): CustomSet {
+    if (!this.contains(element)) {
+      this.values.push(element);
+    }
+
+    return this;
   }
 
-  subset(other: unknown): CustomSet {
-    throw new Error('Remove this statement and implement this function')
+  subset(other: CustomSet): boolean {
+    for (const val of this.values) {
+      if (!other.contains(val)) {
+        return false;
+      }
+    }
+
+    return true;
   }
 
-  disjoint(other: unknown): CustomSet {
-    throw new Error('Remove this statement and implement this function')
+  disjoint(other: CustomSet): boolean {
+    for (const val of this.values) {
+      if (other.contains(val)) {
+        return false;
+      }
+    }
+
+    return true;
   }
 
-  eql(other: unknown): unknown {
-    throw new Error('Remove this statement and implement this function')
+  eql(other: CustomSet): boolean {
+    if (this.values.length !== other.values.length) {
+      return false;
+    }
+
+    for (const val of this.values) {
+      if (!other.contains(val)) {
+        return false;
+      }
+    }
+
+    return true;
   }
 
-  union(other: unknown): CustomSet {
-    throw new Error('Remove this statement and implement this function')
+  union(other: CustomSet): CustomSet {
+    return new CustomSet([...this.values, ...other.values]);
   }
 
-  intersection(other: unknown): CustomSet {
-    throw new Error('Remove this statement and implement this function')
+  intersection(other: CustomSet): CustomSet {
+    const set = new CustomSet();
+
+    for (const val of this.values) {
+      if (other.contains(val)) {
+        set.add(val);
+      }
+    }
+
+    return set;
   }
 
-  difference(other: unknown): CustomSet {
-    throw new Error('Remove this statement and implement this function')
+  difference(other: CustomSet): CustomSet {
+    const set = new CustomSet();
+    
+    for (const val of this.values) {
+      if (!other.contains(val)) {
+        set.add(val);
+      }
+    }
+
+    return set;
   }
 }
