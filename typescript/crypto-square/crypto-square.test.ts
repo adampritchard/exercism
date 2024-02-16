404eeec503b2607a5c8485989e9fa6452757c353
Adam Pritchard <hello@adampritchard.com.au>
2024-02-17 07:10:26 +1000

    typescript; crypto-square; iteration 1

--- typescript/crypto-square/crypto-square.test.ts
+++ typescript/crypto-square/crypto-square.test.ts
@@ -1,5 +1,57 @@
 import { Crypto } from './crypto-square'
 
+describe('Crypto getColCount', () => {
+  it('is correct for length 1', () => {
+    const crypto = new Crypto('');
+    expect(crypto.getColCount(1)).toEqual(1);
+  })
+
+  it('is correct for length 4', () => {
+    const crypto = new Crypto('');
+    expect(crypto.getColCount(4)).toEqual(2);
+  })
+
+  it('is correct for length 6', () => {
+    const crypto = new Crypto('');
+    expect(crypto.getColCount(6)).toEqual(2);
+  })
+
+  it('is correct for length 7', () => {
+    const crypto = new Crypto('');
+    expect(crypto.getColCount(7)).toEqual(3);
+  })
+
+  it('is correct for length 8', () => {
+    const crypto = new Crypto('');
+    expect(crypto.getColCount(8)).toEqual(3);
+  })
+
+  it('is correct for length 9', () => {
+    const crypto = new Crypto('');
+    expect(crypto.getColCount(9)).toEqual(3);
+  })
+
+  it('is correct for length 10', () => {
+    const crypto = new Crypto('');
+    expect(crypto.getColCount(10)).toEqual(3);
+  })
+
+  it('is correct for length 15', () => {
+    const crypto = new Crypto('');
+    expect(crypto.getColCount(15)).toEqual(4);
+  })
+
+  it('is correct for length 16', () => {
+    const crypto = new Crypto('');
+    expect(crypto.getColCount(16)).toEqual(4);
+  })
+
+  it('gets the right col count for length 54', () => {
+    const crypto = new Crypto('');
+    expect(crypto.getColCount(54)).toEqual(7);
+  })
+})
+
 describe('Crypto', () => {
   it('empty plaintext results in an empty ciphertext', () => {
     const crypto = new Crypto('')
@@ -21,17 +73,17 @@ describe('Crypto', () => {
     expect(crypto.ciphertext).toEqual('1')
   })
 
-  xit('9 character plaintext results in 3 chunks of 3 characters', () => {
+  it('9 character plaintext results in 3 chunks of 3 characters', () => {
     const crypto = new Crypto('This is fun!')
     expect(crypto.ciphertext).toEqual('tsf hiu isn')
   })
 
-  xit('8 character plaintext results in 3 chunks, the last one with a trailing space', () => {
+  it('8 character plaintext results in 3 chunks, the last one with a trailing space', () => {
     const crypto = new Crypto('Chill out.')
     expect(crypto.ciphertext).toEqual('clu hlt io ')
   })
 
-  it.skip('54 character plaintext results in 7 chunks, the last two with trailing spaces', () => {
+  it('54 character plaintext results in 7 chunks, the last two with trailing spaces', () => {
     const crypto = new Crypto(
       'If man was meant to stay on the ground, god would have given us roots.'
     )
--- typescript/crypto-square/crypto-square.ts
+++ typescript/crypto-square/crypto-square.ts
@@ -1,9 +1,48 @@
 export class Crypto {
-  constructor(plainText: unknown) {
-    throw new Error('Remove this statement and implement this function')
+  constructor(
+    public plainText: string,
+  ) {}
+
+  get ciphertext(): string {
+    const cleanText = this.plainText
+      .toLowerCase()
+      .replaceAll(/[^\w0-9]/g, '');
+
+    const colCount = this.getColCount(cleanText.length);
+    const rowCount = Math.ceil(cleanText.length / colCount);
+    const square: string[][] = Array.from({ length: rowCount }, () => []);
+
+    let i = 0;
+    for (let col = 0; col < colCount; col += 1) {
+      for (let row = 0; row < rowCount; row += 1) {
+        square[row][col] = cleanText[i] || ' ';
+        i += 1;
+      }
+    }
+
+    return square
+      .map(arr => arr.join(''))
+      .join(' ');
   }
 
-  get ciphertext(): unknown {
-    throw new Error('Remove this statement and implement this function')
+  public getColCount(length: number) {
+    const targetRowCount = Math.ceil(Math.sqrt(length));
+
+    let colCount = 1;
+
+    for (let i = 2; i < length; i += 1) {
+      const rowCount = Math.ceil(length / i);
+
+      if (rowCount === targetRowCount) {
+        colCount = i;
+        break;
+      } else if (rowCount > targetRowCount) {
+        colCount = i;
+      } else {
+        break;
+      }
+    }
+
+    return colCount;
   }
 }
