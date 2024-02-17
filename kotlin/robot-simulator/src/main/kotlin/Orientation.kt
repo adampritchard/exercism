8892aa8cd35960c766404eadfa25c50c3a3b31f2
Adam Pritchard <hello@adampritchard.com.au>
2024-02-18 08:27:10 +1000

    kotlin; robot-simulator; iteration 1

--- kotlin/robot-simulator/src/main/kotlin/Orientation.kt
+++ kotlin/robot-simulator/src/main/kotlin/Orientation.kt
@@ -1,5 +1,21 @@
 enum class Orientation {
+    NORTH {
+        override fun rotatedClockwise(): Orientation = EAST
+        override fun rotatedCounterClockwise(): Orientation = WEST
+    },
+    EAST {
+        override fun rotatedClockwise(): Orientation = SOUTH
+        override fun rotatedCounterClockwise(): Orientation = NORTH
+    },
+    SOUTH {
+        override fun rotatedClockwise(): Orientation = WEST
+        override fun rotatedCounterClockwise(): Orientation = EAST
+    },
+    WEST {
+        override fun rotatedClockwise(): Orientation = NORTH
+        override fun rotatedCounterClockwise(): Orientation = SOUTH
+    };
 
-    NORTH, EAST, SOUTH, WEST
-
+    abstract fun rotatedClockwise(): Orientation
+    abstract fun rotatedCounterClockwise(): Orientation
 }
--- kotlin/robot-simulator/src/main/kotlin/Robot.kt
+++ kotlin/robot-simulator/src/main/kotlin/Robot.kt
@@ -1,8 +1,31 @@
-class Robot {
-
-    // TODO: implement proper constructor, provide read access to `gridPosition` and `orientation`
-
+class Robot(
+    var gridPosition: GridPosition = GridPosition(0, 0),
+    var orientation: Orientation = Orientation.NORTH,
+) {
     fun simulate(instructions: String) {
-        TODO("Implement this function to complete the task")
+        for (instruction in instructions) {
+            when (instruction) {
+                'L' -> rotateLeft()
+                'R' -> rotateRight()
+                'A' -> advance()
+            }
+        }
+    }
+
+    private fun rotateLeft() {
+        orientation = orientation.rotatedCounterClockwise()
+    }
+
+    private fun rotateRight() {
+        orientation = orientation.rotatedClockwise()
+    }
+
+    private fun advance() {
+        gridPosition = when (orientation) {
+            Orientation.NORTH -> GridPosition(gridPosition.x, gridPosition.y + 1)
+            Orientation.EAST -> GridPosition(gridPosition.x + 1, gridPosition.y)
+            Orientation.SOUTH -> GridPosition(gridPosition.x, gridPosition.y - 1)
+            Orientation.WEST -> GridPosition(gridPosition.x - 1, gridPosition.y)
+        }
     }
 }
