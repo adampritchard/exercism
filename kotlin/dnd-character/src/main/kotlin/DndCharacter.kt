23d9eee2215a5924eba815e12fb8e57ce3bdbe41
Adam Pritchard <hello@adampritchard.com.au>
2024-02-18 07:24:04 +1000

    kotlin; dnd-character; iteration 1

--- kotlin/dnd-character/src/main/kotlin/DndCharacter.kt
+++ kotlin/dnd-character/src/main/kotlin/DndCharacter.kt
@@ -1,22 +1,36 @@
-class DndCharacter {
+import kotlin.math.floor
+import kotlin.random.Random
 
-    val strength: Int = TODO("Initialize value to complete the task")
-    val dexterity: Int = TODO("Initialize value to complete the task")
-    val constitution: Int = TODO("Initialize value to complete the task")
-    val intelligence: Int = TODO("Initialize value to complete the task")
-    val wisdom: Int = TODO("Initialize value to complete the task")
-    val charisma: Int = TODO("Initialize value to complete the task")
-    val hitpoints: Int = TODO("Initialize value to complete the task")
+class DndCharacter {
+    val strength: Int = ability()
+    val dexterity: Int = ability()
+    val constitution: Int = ability()
+    val intelligence: Int = ability()
+    val wisdom: Int = ability()
+    val charisma: Int = ability()
+    val hitpoints = 10 + modifier(constitution)
 
     companion object {
+        fun diceRoll() = Random.nextInt(from = 1, until = 7)
 
         fun ability(): Int {
-            TODO("Implement the function to complete the task")
+            val rolls = mutableListOf<Int>(
+                diceRoll(),
+                diceRoll(),
+                diceRoll(),
+                diceRoll(),
+            )
+            rolls.sort()
+            rolls.removeAt(0)
+            return rolls.sum()
         }
 
         fun modifier(score: Int): Int {
-            TODO("Implement the function to complete the task")
+            val mod = (score - 10) / 2.0
+            return when {
+                mod < 0 -> floor(mod).toInt()
+                else -> mod.toInt()
+            }
         }
     }
-
 }
