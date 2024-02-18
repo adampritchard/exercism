367a5dc3ae7f09ac6e384931644401fcd2528385
Adam Pritchard <hello@adampritchard.com.au>
2024-02-18 10:40:17 +1000

    kotlin; bank-account; iteration 1

--- kotlin/bank-account/src/main/kotlin/BankAccount.kt
+++ kotlin/bank-account/src/main/kotlin/BankAccount.kt
@@ -1,11 +1,27 @@
-class BankAccount {
-    // TODO: implement read access to 'balance'
+import java.util.concurrent.atomic.AtomicLong
 
-    fun adjustBalance(amount: Long){
-        TODO("Implement the function to complete the task")
+class BankAccount {
+    val balance: Long
+        get() {
+            assertAccountOpen()
+            return _balance.get()
+        }
+
+    private var _balance = AtomicLong(0)
+    private var isClosed = false
+
+    fun adjustBalance(amount: Long) {
+        assertAccountOpen()
+        _balance.addAndGet(amount)
     }
 
     fun close() {
-        TODO("Implement the function to complete the task")
+        isClosed = true
+    }
+
+    private fun assertAccountOpen() {
+        if (isClosed) {
+            throw IllegalStateException()
+        }
     }
 }
